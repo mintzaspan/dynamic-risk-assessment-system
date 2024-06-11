75c57a8227ec50a432dd46c6e5ced5b90a8622a1
mincorp <mincorp@users.noreply.github.com>
2024-06-11 12:25:01 +0100

    training script

--- training.py
+++ training.py
@@ -1,32 +1,95 @@
-from flask import Flask, session, jsonify, request
 import pandas as pd
-import numpy as np
 import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, TargetEncoder
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LogisticRegression
 import json
 
-###################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+# Function for training the model
+def train_model(data_path, response, model_output_path):
+    """Trains a Logistic Regression model and saves to folder
+
+    Args:
+        data_path : folder where input data CSV is saved
+        response: response variable
+        model_output_path: folder to save trained model
+
+    Returns:
+        None
+    """
+
+    data = pd.read_csv(os.path.join(data_path, "finaldata.csv"))
+    y = data[response]
+    X = data.drop(columns=[response])
+
+    # preprocessing
+    num_features = X.select_dtypes(include='number').columns.tolist()
+    num_transformer = make_pipeline(SimpleImputer(), StandardScaler())
+
+    cat_features = X.select_dtypes(include='object').columns.tolist()
+    cat_transformer = make_pipeline(
+        SimpleImputer(
+            strategy='most_frequent'), TargetEncoder(
+            target_type='binary'), StandardScaler())
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num", num_transformer, num_features),
+            ("cat", cat_transformer, cat_features),
+        ]
+    )
+
+    # use this logistic regression for training
+    logreg = LogisticRegression(
+        C=1.0,
+        class_weight=None,
+        dual=False,
+        fit_intercept=True,
+        intercept_scaling=1,
+        l1_ratio=None,
+        max_iter=100,
+        multi_class='deprecated',
+        n_jobs=None,
+        penalty='l2',
+        random_state=0,
+        solver='liblinear',
+        tol=0.0001,
+        verbose=0,
+        warm_start=False)
+
+    # model pipeline
+    pipe = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor), ("classifier", logreg)])
+
+    # fit the logistic regression to your data
+    pipe.fit(X, y)
+
+    # check if model_output_path exists
+    if not os.path.exists(model_output_path):
+        os.makedirs(model_output_path)
+
+    # write the trained model to your workspace in a file called
+    # trainedmodel.pkl
+    with open(os.path.join(model_output_path, "trainedmodel.pkl"), "wb") as f:
+        pickle.dump(obj=pipe, file=f)
 
 
-#################Function for training the model
-def train_model():
-    
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
-    
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+if __name__ == "__main__":
 
+    # Load config.json and get path variables
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    dataset_csv_path = os.path.join(config['output_folder_path'])
+    model_path = os.path.join(config['output_model_path'])
+
+    # Train model and save as pkl
+    train_model(
+        data_path=dataset_csv_path,
+        response="exited",
+        model_output_path=model_path)
