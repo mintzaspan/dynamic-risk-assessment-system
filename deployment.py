d68ebbe09aa91dd90b4c6a3286fe1c975e5d1d67
mincorp <mincorp@users.noreply.github.com>
2024-06-11 15:34:05 +0100

    deployment script

--- deployment.py
+++ deployment.py
@@ -1,27 +1,43 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
 
 
+# function for deployment
+def deploy_model(model_path, ingested_data_path, deployment_path):
+    """Copys the latest pickle file, the latestscore.txt value, and the ingestedfiles.txt file into the deployment directory
 
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
+    Args:
+        model_path: folder that contains trainedmodel.pkl and latestscore.txt
+        ingested_data_path: folder that contains ingestedfiles.txt
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+    Return:
+        None
+    """
+
+    # check if prod_deployment_path exists
+    if not os.path.exists(deployment_path):
+        os.makedirs(deployment_path)
+
+    os.system(
+        f"cp {os.path.join(model_path, 'trainedmodel.pkl')} {os.path.join(deployment_path, 'trainedmodel.pkl')}")
+    os.system(
+        f"cp {os.path.join(model_path, 'latestscore.txt')} {os.path.join(deployment_path, 'latestscore.txt')}")
+    os.system(
+        f"cp {os.path.join(ingested_data_path, 'ingestedfiles.txt')} {os.path.join(deployment_path, 'ingestedfiles.txt')}")
 
 
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
+if __name__ == "__main__":
 
+    # Load config.json and get path variables
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    dataset_csv_path = os.path.join(config['output_folder_path'])
+    model_path = os.path.join(config['output_model_path'])
+    prod_deployment_path = os.path.join(config['prod_deployment_path'])
+
+    # deploy
+    deploy_model(
+        model_path=model_path,
+        ingested_data_path=dataset_csv_path,
+        deployment_path=prod_deployment_path)
