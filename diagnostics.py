06b746da6a068aaf9534a71b779c2732b1177580
mincorp <mincorp@users.noreply.github.com>
2024-06-12 15:23:29 +0100

    get model predictions

--- diagnostics.py
+++ diagnostics.py
@@ -4,42 +4,76 @@ import numpy as np
 import timeit
 import os
 import json
+import pickle
 
-##################Load config.json and get environment variables
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+# Function to get model predictions
+def model_predictions(deployed_model_path, test_data, response):
+    """Reads the deployed model and a test dataset and returns calculated predictions in a list.
 
-##################Function to get model predictions
-def model_predictions():
-    #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    Args:
+        deployed_model_path: path to trained model marked for production
+        test_data: path to data to predict on
+        response: name of target variable
 
-##################Function to get summary statistics
+    Returns:
+        predictions: list with predictions
+    """
+
+    # load model
+    with open(deployed_model_path, 'rb') as f:
+        model = pickle.load(f)
+
+    # load data
+    test_df = pd.read_csv(test_data)
+    if response in test_df.columns:
+        y_test = test_df[response]
+        X_test = test_df.drop(columns=[response])
+    else:
+        X_test = test_df
+
+    predictions = model.predict(X_test).tolist()
+    print(predictions)
+
+    return (predictions)
+
+# Function to get summary statistics
+
+
 def dataframe_summary():
-    #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    # calculate summary statistics here
+    pass  # return value should be a list containing all summary statistics
 
-##################Function to get timings
+# Function to get timings
+
+
 def execution_time():
-    #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    # calculate timing of training.py and ingestion.py
+    pass  # return a list of 2 timing values in seconds
 
-##################Function to check dependencies
+# Function to check dependencies
+
+
 def outdated_packages_list():
-    #get a list of 
+    pass  # get a list of
 
 
 if __name__ == '__main__':
-    model_predictions()
+
+    # Load config.json and get environment variables
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    # make predictions
+    model_predictions(
+        deployed_model_path=os.path.join(
+            config['prod_deployment_path'],
+            'trainedmodel.pkl'),
+        test_data=os.path.join(
+            config['test_data_path'],
+            'testdata.csv'),
+        response='exited')
+
     dataframe_summary()
     execution_time()
     outdated_packages_list()
-
-
-
-
-
-    
