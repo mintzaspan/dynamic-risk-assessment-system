a446f095c24a1f4774df6d2e03192868d46d102d
mincorp <mincorp@users.noreply.github.com>
2024-07-02 16:54:48 +0100

    full process

--- fullprocess.py
+++ fullprocess.py
@@ -3,6 +3,10 @@ import os
 import glob
 import sys
 import subprocess
+from ingestion import merge_multiple_dataframes
+from training import train_model
+from scoring import score_model
+import pickle
 
 
 # Load config.json and get environment variables
@@ -10,25 +14,25 @@ with open('config.json', 'r') as f:
     config = json.load(f)
 
 
-# # Check and read new data
-# # first, read ingestedfiles.txt
-# with open(os.path.join(config['output_folder_path'], 'ingestedfiles.txt'), 'r') as file:
-#     ingested_files = [line.replace('\n', '') for line in file.readlines()]
+# Check and read new data
+# First, read ingestedfiles.txt
+with open(os.path.join(config['output_folder_path'], 'ingestedfiles.txt'), 'r') as file:
+    ingested_files = [line.replace('\n', '') for line in file.readlines()]
 
-# # second, determine whether the source data folder has files that aren't
-# # listed in ingestedfiles.txt
-# new_files = []
-# for file in glob.glob(os.path.join(config['input_folder_path'], '*.csv')):
-#     if file not in ingested_files:
-#         new_files.append(file)
+# Second, determine whether the source data folder has files that aren't
+# listed in ingestedfiles.txt
+new_files = []
+for file in glob.glob(os.path.join(config['input_folder_path'], '*.csv')):
+    if file not in ingested_files:
+        new_files.append(file)
 
 
-# # Deciding whether to proceed, part 1
-# # if you found new data, you should proceed. otherwise, do end the process here
-# if len(new_files) == 0:
-#     sys.exit(0)
-# else:
-#     subprocess.run(["python", "ingestion.py"])
+# Deciding whether to proceed, part 1
+# if you found new data, you should proceed. otherwise, do end the process here
+if len(new_files) == 0:
+    decision_1 = False
+else:
+    decision_1 = True
 
 # Checking for model drift
 # check whether the score from the deployed model is different from the
@@ -36,25 +40,52 @@ with open('config.json', 'r') as f:
 with open(os.path.join(config['prod_deployment_path'], 'latestscore.txt'), 'r') as file:
     current_score = float(file.read().strip())
 
-# get new score
-subprocess.run(["python", "training.py"])
-subprocess.run(["python", "scoring.py"])
+# # # get new score
+if not os.path.exists('check/'):
+    os.makedirs('check/')
+new_files, new_data = merge_multiple_dataframes(
+    input_path=config['input_folder_path'])
+new_data.to_csv('check/new_data.csv', index=False)
+new_model = train_model('check/new_data.csv', response='exited')
+with open("check/trainedmodel.pkl", "wb") as f:
+    pickle.dump(obj=new_model, file=f)
+new_score = score_model('check/trainedmodel.pkl', os.path.join(
+    config['test_data_path'],
+    'testdata.csv'),
+    'exited')
+subprocess.run(['rm', '-rf', 'check/'])
 
-with open(os.path.join(config['output_model_path'], 'latestscore.txt'), 'r') as file:
-    new_score = float(file.read().strip())
-
-# Deciding whether to proceed, part 2
-# if you found model drift, you should proceed. otherwise, do end the
-# process here
-if new_score <= current_score:
-    sys.exit(0)
-# Re-deployment
-# if you found evidence for model drift, re-run the deployment.py script
+# Proceed or not, part two
+if new_score > current_score:
+    decision_2 = True
 else:
-    subprocess.run(["python", "deployment.py"])
-    # Diagnostics and reporting
-    # run diagnostics.py and reporting.py for the re-deployed model
-    subprocess.run(["python", "diagnostics.py"])
-    subprocess.run(["python", "reporting.py"])
-    subprocess.run(["python", "app.py"])
-    subprocess.run(["python", "apicalls.py"])
+    decision_2 = False
+
+if (decision_1 & decision_2):
+    subprocess.run(['python', 'ingestion.py'])
+    subprocess.run(['python', 'training.py'])
+    subprocess.run(['python', 'scoring.py'])
+    subprocess.run(['python', 'deployment.py'])
+    subprocess.run(['python', 'reporting.py'])
+    subprocess.run(['python', 'app.py'])
+    subprocess.run(['python', 'apicalls.py'])
+
+
+# # with open(os.path.join(config['output_model_path'], 'latestscore.txt'), 'r') as file:
+# #     new_score = float(file.read().strip())
+
+# # Deciding whether to proceed, part 2
+# # if you found model drift, you should proceed. otherwise, do end the
+# # process here
+# if new_score <= current_score:
+#     sys.exit(0)
+# # Re-deployment
+# # if you found evidence for model drift, re-run the deployment.py script
+# else:
+#     subprocess.run(["python", "deployment.py"])
+#     # Diagnostics and reporting
+#     # run diagnostics.py and reporting.py for the re-deployed model
+#     subprocess.run(["python", "diagnostics.py"])
+#     subprocess.run(["python", "reporting.py"])
+#     subprocess.run(["python", "app.py"])
+#     subprocess.run(["python", "apicalls.py"])
